622794ea69854a180f445135ef162adbb4285c35
AbhishekGoswami <https://github.com/agoswami2299-lgtm/LEARN_PLAYWRIGHT_BATCH_2X>
2026-05-13 01:19:38 +0530

    Add Chapter 02 Javascript Concept and Chapter 03 Identifier Literals

---
+++ Chapter_03_Identifier_Literlas/06_Identifier_Rules.js
@@ -0,0 +1,37 @@
+//Basiccally how the Idetifier will start
+var $ = 29;
+var _a = 30;
+var a = 54;
+
+//It will start with the nummbers
+//var 123 = 123;
+
+var abc123 = 123;
+
+var name = 'Abhishek Goswami';
+var name = 'Kensika Bekele';
+
+//var Abhishek Goswami = 'Hellow';
+
+var TYN9879  = 'Hellow';
+var Abhishek_Goswami = 'Start with the letter';
+var Abhishek$Goswami  = 'Start with the dollar sign';
+var abc123  = 'letter and the digit';
+var _Honey245 = 'Undescore letter then digit';
+var $ajgd_12 = 'dollar letter Undescore and digit Mixed';
+
+/* Identifier Rules:
+1. Must not be a reserved keyword  
+   let function = 1; ❌
+2. Can contain letters, digits, underscores, and dollar signs  
+   user_name$1
+3. Must start with a letter, underscore, or dollar sign (not a digit)  
+   1name ❌
+4. Case-sensitive  
+   Name and name are different
+5. No spaces allowed  
+   user name ❌
+6. Unicode letters are allowed  
+   用户
+7. Cannot contain special characters except $ and _  
+   user-name ❌*/
---
+++ Chapter_03_Identifier_Literlas/Comments.js
@@ -0,0 +1,11 @@
+// line comment this will be ignore
+// this line will be not exeuted
+
+/* Ths abhishek goswami from checmbur mumbai*/
+/* Ths abhishek goswami from checmbur mumbai*/
+
+
+
+
+
+         dfsdfdf
